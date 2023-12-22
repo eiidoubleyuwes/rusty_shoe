0e9f57351f7fff9a57af05bb51efa2683278f24c
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2023-12-22 13:54:49 +0300

    Created a simple calculator app in Rust

---
+++ day1/src/main.rs
@@ -0,0 +1,26 @@
+    //Creating a calculator app
+use std::io;
+fn main() {
+    println!("Welcome to simple calculator,it can only however do two numbers at a time");
+    println!("Enter the first number");
+    let mut num1 = String::new();
+    io::stdin().read_line(&mut num1).expect("Number expected");
+    let num1: f32 = num1.trim().parse().expect("Number expected");
+    println!("Enter your operator(+,-,*,/,%)");
+    let mut operator = String::new();
+    io::stdin().read_line(&mut operator).expect("Operator expected");
+    let operator: char = operator.trim().parse().expect("Operator expected");
+    println!("Enter the second number");
+    let mut num2 = String::new();
+    io::stdin().read_line(&mut num2).expect("Number expected");
+    let num2: f32 = num2.trim().parse().expect("Number expected");
+    match operator {
+        '+' => println!("{}", num1 + num2),
+        '-' => println!("{}", num1 - num2),
+        '*' => println!("{}", num1 * num2),
+        '/' => println!("{}", num1 / num2),
+        '%' => println!("{}", num1 % num2),
+        _ => println!("Invalid operator"),
+    
+
+}
