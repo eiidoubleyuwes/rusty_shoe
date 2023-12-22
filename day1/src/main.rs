bd79cdebb3e64063837abbcb2626ce2b93a1ef34
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2023-12-22 13:58:38 +0300

    Fixed error with the code,missing closing bracket for function

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     println!("Enter the first number");
     let mut num1 = String::new();
     io::stdin().read_line(&mut num1).expect("Number expected");
+    //Convert num1 to float and trim the enter key
     let num1: f32 = num1.trim().parse().expect("Number expected");
     println!("Enter your operator(+,-,*,/,%)");
     let mut operator = String::new();
@@ -13,6 +14,7 @@ fn main() {
     println!("Enter the second number");
     let mut num2 = String::new();
     io::stdin().read_line(&mut num2).expect("Number expected");
+    //Convert num2 to float and trim the enter key
     let num2: f32 = num2.trim().parse().expect("Number expected");
     match operator {
         '+' => println!("{}", num1 + num2),
@@ -21,6 +23,5 @@ fn main() {
         '/' => println!("{}", num1 / num2),
         '%' => println!("{}", num1 % num2),
         _ => println!("Invalid operator"),
-    
-
+    }
 }
