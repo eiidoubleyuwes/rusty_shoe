6fd6982495356e247e83e17056338bcba3ddf565
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-01 19:30:12 +0300

    Feat:Print f1_teams in hashmap

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -2,6 +2,12 @@
 use std::collections::HashMap;
 fn main() {
     let mut f1_teams = HashMap::new();
-    f1_teams.insert("Mercedes", "Red Bull", "Ferrari", "McLaren", "Aston Martin", "Alpine", "AlphaTauri", "Alfa Romeo", "Haas", "Williams");
-    println!("The F1 teams are: {}", f1_teams);
+    f1_teams.insert("Mercedes", "Red Bull");
+    f1_teams.insert("Ferrari", "McLaren");
+    f1_teams.insert("Aston Martin", "Alpine");
+    f1_teams.insert("AlphaTauri", "Alfa Romeo"); 
+    f1_teams.insert("Haas", "Williams");
+    println!("{:?}", f1_teams);
+    println!("The number of teams in F1 is {}", f1_teams.len());
+   
 }
