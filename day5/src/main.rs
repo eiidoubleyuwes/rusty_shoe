b928a2fd1184340f55e5309556b13094fc90e9ed
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-01 19:22:47 +0300

    Feat:Added F1 teams to hashmap

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -2,4 +2,6 @@
 use std::collections::HashMap;
 fn main() {
     let mut f1_teams = HashMap::new();
+    f1_teams.insert("Mercedes", "Red Bull", "Ferrari", "McLaren", "Aston Martin", "Alpine", "AlphaTauri", "Alfa Romeo", "Haas", "Williams");
+    println!("The F1 teams are: {}", f1_teams);
 }
