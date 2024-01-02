0fc1ec9d5c6d28508153764a2b7fee408c7922cd
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-02 19:59:26 +0300

    Feat:Made it f1 based

--- day6_inventory/src/main.rs
+++ day6_inventory/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+//Simple inventory management program for an f1 team
+struct pit_crew{
+    name: String,
+    team: String,
+    points: u32,
+}
+
+fn get_pit_crew(name: String, team: String, points: u32) -> pit_crew{
+    pit_crew{
+        name,
+        team,
+        points,
+    }
+}
+fn add_pit_crew(pit_crew: &mut Vec<pit_crew>, name: String, team: String, points: u32){
+    pit_crew.push(get_pit_crew(name, team, points)); //move to next owner
 }
