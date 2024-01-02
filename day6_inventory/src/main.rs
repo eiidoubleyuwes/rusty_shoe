e40b6565d101117ec30656fd7120f14456d33aee
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-02 20:01:33 +0300

    Feat:Added new function

--- day6_inventory/src/main.rs
+++ day6_inventory/src/main.rs
@@ -15,3 +15,8 @@ fn get_pit_crew(name: String, team: String, points: u32) -> pit_crew{
 fn add_pit_crew(pit_crew: &mut Vec<pit_crew>, name: String, team: String, points: u32){
     pit_crew.push(get_pit_crew(name, team, points)); //move to next owner
 }
+fn print_pit_crew(pit_crew: &Vec<pit_crew>){
+    for i in pit_crew{
+        println!("Name: {} Team: {} Points: {}", i.name, i.team, i.points);
+    }
+}
