0a2bde32f83177cf14dfeb570e78f8453ece28c7
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-03 20:03:43 +0300

    Feat:Added Dependancies to the toml file

--- day7_f1registry/src/main.rs
+++ day7_f1registry/src/main.rs
@@ -1,3 +1,4 @@
-fn main() {
-    println!("Hello, world!");
-}
+//This is a simple project to allow teams to register for the formula 1 grid
+//This project has the same mechanics as a to-do list app
+//This code is also using dependancies such as serde_json and Nickel
+
