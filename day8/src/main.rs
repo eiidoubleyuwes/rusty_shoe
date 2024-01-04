cf31894e70d771e2476ff3a3cb4e6d85aa83d457
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-04 19:09:11 +0300

    Feat:Learnt about string literals

---
+++ day8/src/main.rs
@@ -0,0 +1,6 @@
+//Today is a chill day I'm just learning about string literals and string manipulatuion
+fn main() {
+    let mut s = String::from("Formula 1");
+    s.push_str(" is the best sport");
+    println!("{}", s);
+}
