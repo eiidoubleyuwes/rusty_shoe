7f4f957f2f30269ae9bc6034cdecf4b3f23ed12d
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-08 17:32:45 +0300

    Feat:Learnt about Serde

---
+++ day10/src/main.rs
@@ -0,0 +1,9 @@
+use serde_json;
+use std::fs;
+
+fn main() {
+    let path = "./src/input.json";
+    let data = fs::read_to_string(path).expect("Unable to read file");
+    let res: serde_json::Value = serde_json::from_str(&data).expect("Unable to parse");
+    println!("{}", res);
+}
