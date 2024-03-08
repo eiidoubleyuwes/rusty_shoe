dfc3e7b0db050e64f2fbd4ffbaf793ddce7ad88f
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-08 19:09:11 +0300

    Feat:IDK

--- guessing_color/src/main.rs
+++ guessing_color/src/main.rs
@@ -4,19 +4,23 @@ use colored::*;
 use std::cmp::Ordering;
 fn main() {
    println!("Hello, welcome to the guesssing game");
+   loop{
+   let secret = rand::thread_rng().gen_range(1..1001);
    println!("Please enter a number");
+   println!("The random number is: {}" , secret);
    let mut guess = String::new();
    io::stdin()
    .read_line(&mut guess)
    .expect("Failed to read line");
    println!("You guessed : {}" , guess);
-   let guess: u32 = guess.trim().parse().expect("Not a number");
-   let secret = rand::thread_rng().gen_range(1..1001);
-   println!("The number was: {}" , secret);
+   let guess: u32 = match guess.trim().parse() {
+	Ok(num) =>num,
+	Err(_) => continue};
    match guess.cmp(&secret) {
 	Ordering::Less => println!("{}","You lose,too low" .red()),
 	Ordering::Greater =>println!("{}","You lose,too high" .red()),
-	Ordering::Equal => println!("{}","You won!" .green()),
+	Ordering::Equal => {println!("{}","You won!" .green()); break;},
+}
 }
 }
 
