613eab73aaf9d254f8b7523e8bcf465ee8ea3a2b
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-11 21:41:12 +0300

    Feat:Added default boiler plate code
    This was simple enough to do

--- rusty_aws/src/main.rs
+++ rusty_aws/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use aws_config::meta::region::RegionProviderChain;
+use aws_config::BehaviorVersion;
+use aws_sdk_dynamodb::{Client, Error};
+
+/// Lists your DynamoDB tables in the default Region or us-east-1 if a default Region isn't set.
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let config = aws_config::defaults(BehaviorVersion::latest())
+        .region(region_provider)
+        .load()
+        .await;
+    let client = Client::new(&config);
+
+    let resp = client.list_tables().send().await?;
+
+    println!("Tables:");
+
+    let names = resp.table_names();
+
+    for name in names {
+        println!("  {}", name);
+    }
+
+    println!();
+    println!("Found {} tables", names.len());
+
+    Ok(())
 }
+
