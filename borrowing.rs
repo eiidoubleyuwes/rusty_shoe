112d1cae5c70dd8a2faa0e8ee5819ca24dd29ea2
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-11 20:45:53 +0300

    Feat:Added user input, This is importatnt to understand pointers and references

--- borrowing.rs
+++ borrowing.rs
@@ -1,9 +1,18 @@
-fn main () {
-    let s1 : String = String::from("Hello");
-    let len = calculate(&s1);
-    println!("the length of {} is {}" ,s1 ,len);
+use std::io;
+fn main() {
+loop {
+    println!("Enter a string to get the length of the string");
+    let mut s1 = String::new(); // Create a new, mutable String
+
+    io::stdin()
+        .read_line(&mut s1) // Read input into s1
+        .expect("Failed to read line");
+
+    let len = calculate(&s1); // Pass the reference to s1 to calculate
+    println!("The length of {} is {}", s1, len);
 }
-fn calculate (s: &String) -> usize {
-    let length = s.len();
+}
+fn calculate(s: &String) -> usize {
+    let length = s.len(); // Calculate length using the reference
     length
 }
