dafaec4940cb396e50839ff21bdd3ff4fd7454a0
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-11 21:22:45 +0300

    Feat:Learning slices
    As of now the function to check the second string is a bit faulty

--- slice.rs
+++ slice.rs
@@ -1,15 +1,29 @@
 //Here I am learning slices in Rust
 fn main(){
-    let mut cut_me = String::from("Hello World");
+    //Allow user input
+    println!("Enter a string to get the first word and second word: ");
+    let mut cut_me = String::new();
+    std::io::stdin().read_line(&mut cut_me).unwrap();
+    println!("You entered: {}", cut_me);
     let word = first_word(&cut_me);
-    cut_me.clear();
+    let maneno = second_word(&cut_me);
     println!("The first word is: {}", word);
+    println!("The second word is: {}", maneno);
 }
-fn first_word(s: &String) -> &str{
+fn first_word(s: &str) -> &str{
     let bytes = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate(){
-        if item == b' '{ //Checks for empty space
-            return &s[0..i];
+        if item == b' '{
+            return &s[0..i]; //Return  a slice to the whitespace
+        }
+    }
+    &s[..]
+}
+fn second_word(s: &str) -> &str{
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return &s[i+1..]; //Return  a slice to the whitespace
         }
     }
     &s[..]
